24068dfa46dfe73c0645824d30578cc86adcef61
Gustavo Bodra <gustavobodra@yahoo.com.br>
2020-10-21 23:23:57 -0300

    Final da implementacao do LDA. Faltando implementar o PCA e comparar as retas

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ import numpy as np
 def means(x, y):
     means_vector = []
     for c in range(3):
-        x1_mean = sum(x[y == c, 0]) / len(x[y == c, 0])
-        x2_mean = sum(x[y == c, 1]) / len(x[y == c, 1])
-        x3_mean = sum(x[y == c, 2]) / len(x[y == c, 2])
-        x4_mean = sum(x[y == c, 3]) / len(x[y == c, 3])
-        means_vector.append([x1_mean, x2_mean, x3_mean, x4_mean])
+        x1_y_mean = sum(x[y == c, 0]) / len(x[y == c, 0])
+        x2_y_mean = sum(x[y == c, 1]) / len(x[y == c, 1])
+        x3_y_mean = sum(x[y == c, 2]) / len(x[y == c, 2])
+        x4_y_mean = sum(x[y == c, 3]) / len(x[y == c, 3])
+        means_vector.append([x1_y_mean, x2_y_mean, x3_y_mean, x4_y_mean])
 
-    x_zipped = [item for sublist in x for item in sublist]
-    overall_mean = []
-    
-    overall_mean.append()
+    x1_mean = sum(x[:, 0]) / len(x[:, 0])
+    x2_mean = sum(x[:, 1]) / len(x[:, 1])
+    x3_mean = sum(x[:, 2]) / len(x[:, 2])
+    x4_mean = sum(x[:, 3]) / len(x[:, 3])
+    overall_mean = [x1_mean, x2_mean, x3_mean, x4_mean]
+
     return means_vector, overall_mean
 
 
@@ -148,6 +150,47 @@ def matrix_multiply(a, b):
     return result_matrix
 
 
+def matrix_minor(m, i, j):
+    return [row[:j] + row[j + 1:] for row in (m[:i] + m[i + 1:])]
+
+
+def matrix_determinant(m):
+    # caso especial para matriz 2x2
+    if len(m) == 2:
+        return m[0][0] * m[1][1] - m[0][1] * m[1][0]
+
+    determinant = 0
+    for c in range(len(m)):
+        determinant += ((-1) ** c) * m[0][c] * matrix_determinant(matrix_minor(m, 0, c))
+    return determinant
+
+
+def matrix_inverse(m):
+    determinant = matrix_determinant(m)
+
+    # caso especial para matriz 2x2
+    if len(m) == 2:
+        return [[m[1][1] / determinant, -1 * m[0][1] / determinant],
+                [-1 * m[1][0] / determinant, m[0][0] / determinant]]
+
+    # calcular matriz de cofatores
+    cofactors = []
+
+    for r in range(len(m)):
+        cofactorRow = []
+        for c in range(len(m)):
+            minor = matrix_minor(m, r, c)
+            cofactorRow.append(((-1) ** (r + c)) * matrix_determinant(minor))
+        cofactors.append(cofactorRow)
+
+    cofactors = matrix_transpose(cofactors)
+
+    for r in range(len(cofactors)):
+        for c in range(len(cofactors)):
+            cofactors[r][c] = cofactors[r][c] / determinant
+    return cofactors
+
+
 def scatter_within(x, y, mean):
     sw = matrix_empty(4, 4)
     for cl, mv in zip(range(3), mean):
@@ -168,21 +211,91 @@ def scatter_within(x, y, mean):
 
 def scatter_between(x, y, means_vector, overall_mean):
     sb = matrix_empty(4, 4)
-    overall_mean = np.mean(x, axis=0)
+    overall_mean = matrix_transpose(overall_mean)
 
     for i, mean_vec in enumerate(means_vector):
         n = len(x[y == i, :])
         mean_vec = vector_to_matrix(mean_vec)
-        # mean_vec = mean_vec.reshape(4, 1)  # make column vector
-        # overall_mean = vector_to_matrix([overall_mean])
-        overall_mean = overall_mean.reshape(4, 1)  # make column vector
         mean_vec_minus_overall_mean = matrix_subtraction(mean_vec, overall_mean)
         mean_vec_minus_overall_mean_t = matrix_transpose(mean_vec_minus_overall_mean)
         multiply = matrix_multiply(mean_vec_minus_overall_mean, mean_vec_minus_overall_mean_t)
+        sb_n = [[item * n for item in item_list] for item_list in multiply]
+        sb = matrix_addition(sb, sb_n)
 
-        sb += n * (mean_vec - overall_mean).dot((mean_vec - overall_mean).T)
+    return sb
 
-    print('between-class Scatter Matrix:\n', sb)
+
+def sw_sb(sw, sb):
+    sw_i = matrix_inverse(sw)
+    sw_i_sb = matrix_multiply(sw_i, sb)
+    eig_vals, eig_vecs = np.linalg.eig(sw_i_sb)
+
+    return eig_vals, eig_vecs
+
+
+def sort_eig(eig_vals, eig_vecs):
+    # STEP 4 - Selecting linear discriminants for the new feature subspace
+
+    # Make a list of (eigenvalue, eigenvector) tuples
+    eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:, i]) for i in range(len(eig_vals))]
+
+    # Sort the (eigenvalue, eigenvector) tuples from high to low
+    eig_pairs = sorted(eig_pairs, key=lambda k: k[0], reverse=True)
+
+    # Visually confirm that the list is correctly sorted by decreasing eigenvalues
+
+    print('Eigenvalues in decreasing order:\n')
+    for i in eig_pairs:
+        print(i[0])
+
+    print('Variance explained:\n')
+    eigv_sum = sum(eig_vals)
+    for i, j in enumerate(eig_pairs):
+        print('eigenvalue {0:}: {1:.2%}'.format(i + 1, (j[0] / eigv_sum).real))
+
+    # STEP 4.2. Choosing k eigenvectors with the largest eigenvalues
+    W = np.hstack((eig_pairs[0][1].reshape(4, 1), eig_pairs[1][1].reshape(4, 1)))
+    print('Matrix W:\n', W.real)
+
+    # STEP 5 Transforming the samples onto the new subspace
+    X_lda = iris_data.dot(W)
+    assert X_lda.shape == (150, 2), "The matrix is not 150x2 dimensional."
+
+    return X_lda
+
+
+def plot_step_lda():
+    ax = plt.subplot(111)
+    for label, marker, color in zip(
+            range(0, 3), ('^', 's', 'o'), ('blue', 'red', 'green')):
+        plt.scatter(x=X_lda[:, 0].real[iris_target == label],
+                    y=X_lda[:, 1].real[iris_target == label],
+                    marker=marker,
+                    color=color,
+                    alpha=0.5,
+                    label=label_dict[label]
+                    )
+
+    plt.xlabel('LD1')
+    plt.ylabel('LD2')
+
+    leg = plt.legend(loc='upper right', fancybox=True)
+    leg.get_frame().set_alpha(0.5)
+    plt.title('LDA: Iris projection onto the first 2 linear discriminants')
+
+    # hide axis ticks
+    plt.tick_params(axis="both", which="both", bottom="off", top="off",
+                    labelbottom="on", left="off", right="off", labelleft="on")
+
+    # remove axis spines
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+
+    plt.grid()
+    plt.tight_layout
+    plt.show()
 
 
 def plot_data():
@@ -195,9 +308,14 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 iris_dataset = datasets.load_iris()
 iris_data = iris_dataset.data
 iris_target = iris_dataset.target
+label_dict = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
 
 means_per_class, grand_mean = means(iris_data, iris_target)
 s_w = scatter_within(iris_data, iris_target, means_per_class)
-scatter_between(iris_data, iris_target, means_per_class, grand_mean)
+s_b = scatter_between(iris_data, iris_target, means_per_class, grand_mean)
+v, w = sw_sb(s_w, s_b)
+X_lda = sort_eig(v, w)
 
-plot_data()
+plot_step_lda()
+
+# plot_data()
